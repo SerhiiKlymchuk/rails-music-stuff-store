177b7eb8cd41ce3ffe3d96ce393b11eb28320f04
Serhii Klymchuk <serhii.klymchuk23@gmail.com>
2021-12-09 16:21:20 +0200

    [RUBY] - 12 - implement pay card types + implement email templates

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,24 @@
+class OrderMailer < ApplicationMailer
+  default from: 'MusicStuff Mailer <musicstuff.mailer@example.com>'
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.received.subject
+  #
+  def received(order)
+    @order = order
+    mail to: order.email, subject: 'MusicStuff Store Order Confirmation!'
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.shipped.subject
+  #
+  def shipped(order)
+    @order = order
+    mail to: order.email, subject: 'MusicStuff Store Order Shipped!'
+  end
+
+end
